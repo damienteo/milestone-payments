2efc8e255471d1232da43b8d86389997ae35b578
damienteo <damien.teo.ks@gmail.com>
2023-06-21 22:57:08 +0800

    Update payments page

--- components/views/Payments.tsx
+++ components/views/Payments.tsx
@@ -1,16 +1,186 @@
-import React from "react";
-import { Box, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Box, BoxProps, Button, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { useDispatch, useSelector } from "react-redux";
 
-import Layout from "../layout/Layout";
+import StyledCircularProgress from "../common/StyledCircularProgress";
 
-const MainPage: React.FunctionComponent = () => {
+import { AppDispatch, RootState } from "../../store";
+import usePaymentTransactions from "../../utils/hooks/useMerkleTransactions";
+import useConnectWallet from "../../utils/hooks/useConnectWallet";
+import { setLoading } from "../../features/TransactionsSlice";
+import { getMerkleProof, generateMerkleTree } from "../../utils/merklePayments";
+import { parseTokenValue } from "../../utils/common";
+import { PAYMENT_DETAILS } from "../../constants/common";
+import { IPaymentDetails } from "../../interfaces/IPayment";
+
+const nextPaymentDetails = (): IPaymentDetails => {
+  const nextPayment = JSON.parse(JSON.stringify(PAYMENT_DETAILS));
+  for (let key of Object.keys(nextPayment.Payment)) {
+    nextPayment.Payment[key] = nextPayment.Payment[key] * 2;
+  }
+  return nextPayment;
+};
+
+const StyledBox = styled(Box)<BoxProps>(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  margin: theme.spacing(2, 0),
+}));
+
+const InteractButton = (props: {
+  text: string;
+  method: () => void;
+  loading: boolean;
+  disabled?: boolean;
+}) => {
+  const { text, method, loading, disabled = false } = props;
   return (
-    <Layout>
-      <Box>
-        <Typography>Payments</Typography>
-      </Box>
-    </Layout>
+    <Button variant="outlined" onClick={method} disabled={loading || disabled}>
+      {text}
+      {loading && <StyledCircularProgress size={24} />}
+    </Button>
   );
 };
 
-export default MainPage;
+const CumulativePayment: React.FunctionComponent = () => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  const { account, requestConnect } = useConnectWallet();
+  const { checkPastClaim, setNextMerkleRoot, checkWalletBalance } =
+    usePaymentTransactions();
+
+  const transactionSlice = useSelector((state: RootState) => state.transaction);
+  const { loading } = transactionSlice;
+
+  const PaymentSlice = useSelector((state: RootState) => state.Payment);
+  const { pastClaimed, merkleRoot, walletBalance } = PaymentSlice;
+
+  const [PaymentDetails, setPaymentDetails] =
+    useState<IPaymentDetails>(PAYMENT_DETAILS);
+
+  const setupInitial = async () => {
+    dispatch(setLoading(true));
+
+    await checkPastClaim();
+    await checkWalletBalance();
+
+    dispatch(setLoading(false));
+  };
+
+  const handleSetMerkleRoot = async () => {
+    if (!account) return;
+
+    dispatch(setLoading(true));
+
+    const nextTree = generateMerkleTree(PaymentDetails);
+    const hash = nextTree.getHexRoot();
+
+    await setNextMerkleRoot(hash);
+
+    dispatch(setLoading(false));
+
+    setPaymentDetails(nextPaymentDetails);
+  };
+
+  const resetMerkleRoot = async () => {
+    if (!account) return;
+
+    dispatch(setLoading(true));
+
+    const nextTree = generateMerkleTree(PAYMENT_DETAILS);
+    const hash = nextTree.getHexRoot();
+
+    await setNextMerkleRoot(hash);
+
+    dispatch(setLoading(false));
+
+    setPaymentDetails(PAYMENT_DETAILS);
+  };
+
+  const handleClaim = async () => {
+    if (!account) return;
+
+    dispatch(setLoading(true));
+
+    const amount = parseTokenValue(
+      PaymentDetails.payments[account].toString(),
+      18
+    );
+    const proof = getMerkleProof(
+      account,
+      PaymentDetails.payments[account],
+      PaymentDetails
+    );
+
+    await checkPastClaim();
+    await checkWalletBalance();
+
+    dispatch(setLoading(false));
+  };
+
+  useEffect(() => {
+    if (account) {
+      setupInitial();
+    }
+  }, [account]);
+
+  return (
+    <>
+      {/* Show button to connect if not connected */}
+      {!account && (
+        <StyledBox>
+          <InteractButton
+            text="Connect"
+            method={requestConnect}
+            loading={loading}
+          />
+        </StyledBox>
+      )}
+
+      {account && (
+        <>
+          {/* Show JSON file for the Payment details */}
+          <Box
+            sx={{
+              display: "inline-block",
+              paddingX: 3,
+              border: "1px solid #D3D3D3",
+              borderRadius: 5,
+              marginBottom: 3,
+            }}
+          >
+            <Typography
+              variant="h6"
+              sx={{ display: "inline-block", textAlign: "left" }}
+            >
+              {" "}
+              <pre>{JSON.stringify(PaymentDetails, null, 4)}</pre>
+            </Typography>
+            <Box sx={{ marginBottom: 2 }}>
+              <InteractButton
+                text="Set New Merkle Root"
+                method={handleSetMerkleRoot}
+                loading={loading}
+              />
+              <InteractButton
+                text="Reset Merkle Root"
+                method={resetMerkleRoot}
+                loading={loading}
+              />
+            </Box>
+          </Box>
+
+          <Typography variant="h3">Claimed: {pastClaimed}</Typography>
+          {/* Show the merkle root */}
+          <Typography variant="h5">Merkle Root: {merkleRoot}</Typography>
+          {/* Show current balance */}
+          <Typography variant="h5">$FRG Balance: {walletBalance}</Typography>
+          <InteractButton text="Claim" method={handleClaim} loading={loading} />
+        </>
+      )}
+    </>
+  );
+};
+
+export default CumulativePayment;
