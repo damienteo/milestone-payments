c935857629deed6e196d9a9db8960932ccdac785
damienteo <damien.teo.ks@gmail.com>
2023-06-21 16:47:50 +0800

    Turn off texttransform for buttons

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,12 @@
 import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
-const theme = createTheme({});
+const theme = createTheme({
+  typography: {
+    button: {
+      textTransform: "none",
+    },
+  },
+});
 
 export default responsiveFontSizes(theme);
 
